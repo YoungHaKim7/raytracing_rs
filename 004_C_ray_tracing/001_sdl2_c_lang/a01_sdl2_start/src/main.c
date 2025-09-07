73902d56afab18ce9423be19961f44581257674d
GYoung <ytok1108@aol.com>
2025-09-08 01:00:26 +0900

    C 언어 ray tracing

---
+++ 004_C_ray_tracing/001_sdl2_c_lang/a01_sdl2_start/src/main.c
@@ -0,0 +1,10 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+#define WIDTH 900
+#define HEIGHT 600
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+}
---
+++ 004_C_ray_tracing/README.md
@@ -0,0 +1,2 @@
+# Coding Ray Tracing in C | HirschDaniel
+- https://youtu.be/2BLRLuczykM?si=FNj4XSXVHhjXnyZA
