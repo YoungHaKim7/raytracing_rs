37d15dfbe0e34e0f46680a353681c18030a8f55a
GYoung <ytok1108@aol.com>
2025-09-08 01:06:51 +0900

    sdl2 window

--- 004_C_ray_tracing/001_sdl2_c_lang/a01_sdl2_start/src/main.c
+++ 004_C_ray_tracing/001_sdl2_c_lang/a01_sdl2_start/src/main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 
 #define WIDTH 900
@@ -7,4 +8,9 @@
 int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Raytracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+
+    SDL_Delay(5000);
+    SDL_Quit();
+    return 0;
 }
