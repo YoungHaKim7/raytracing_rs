44670d234f1e5a30a9b8e36c6d6431c5939e827e
GYoung <ytok1108@aol.com>
2025-09-07 22:07:56 +0900

    cpp ray

---
+++ 002_Cpp_ray_tracing/a01_hello_ppm/src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ofstream out("image.ppm");
+    if (!out) {
+        std::cerr << "Error: could not open file\n";
+        return 1;
+    }
+
+    int width = 3;
+    int height = 2;
+
+    // Write PPM header
+    out << "P3\n" << width << " " << height << "\n255\n";
+
+    // Pixel data (row-major order)
+    int pixels[6][3] = {
+        {255, 0, 0},     // red
+        {0, 255, 0},     // green
+        {0, 0, 255},     // blue
+        {255, 255, 0},   // yellow
+        {255, 255, 255}, // white
+        {0, 0, 0}        // black
+    };
+
+    for (int i = 0; i < 6; i++) {
+        out << pixels[i][0] << " "
+            << pixels[i][1] << " "
+            << pixels[i][2] << "\n";
+    }
+
+    out.close();
+    return 0;
+}
+
