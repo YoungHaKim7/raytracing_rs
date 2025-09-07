2792a80368e7e0df913e8aa41ac39409e1ec6aff
GYoung <ytok1108@aol.com>
2025-09-08 00:27:48 +0900

    zig ver 0.15

--- 003_Zig_ray_tracing/day1/a01_day1/src/main.zig
+++ 003_Zig_ray_tracing/day1/a01_day1/src/main.zig
@@ -1,23 +1,26 @@
 const std = @import("std");
 const a01_day1 = @import("a01_day1");
 
+const WIDTH = 256;
+const HEIGHT = 256;
+
 pub fn main() !void {
     var wbuf: [4096]u8 = undefined;
     var file_writer = std.fs.File.stdout().writer(&wbuf);
     const out = &file_writer.interface;
 
-    const height = 256;
-    const width = 256;
+    // const height = 256;
+    // const width = 256;
 
-    try out.print("P3\n{d} {d}\n255\n", .{ width, height });
+    try out.print("P3\n{d} {d}\n255\n", .{ WIDTH, HEIGHT });
 
-    for (0..height) |h| {
-        for (0..width) |w| {
+    for (0..HEIGHT) |h| {
+        for (0..WIDTH) |w| {
             const fh: f32 = @floatFromInt(h);
             const fw: f32 = @floatFromInt(w);
 
-            const r: u8 = @intFromFloat(255.99 * (fh / (height - 1.0)));
-            const g: u8 = @intFromFloat(255.99 * (fw / (width - 1.0)));
+            const r: u8 = @intFromFloat(255.99 * (fh / (HEIGHT - 1.0)));
+            const g: u8 = @intFromFloat(255.99 * (fw / (WIDTH - 1.0)));
             const b: u8 = 0;
 
             try out.print("{d} {d: >3} {d: >3}", .{ r, g, b });
